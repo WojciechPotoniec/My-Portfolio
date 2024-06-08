5350e193dc706df0cb10227349470d9c60fd0b81
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-08 19:20:17 +0200

    change function to get slug

--- app/Models/Type.php
+++ app/Models/Type.php
@@ -12,10 +12,13 @@ class Type extends Model
     use HasFactory;
 
     protected $fillable = ['name', 'slug'];
-    public static function generateSlug($name)
-    {
-        $slug = Str::slug($name, '-');
-
+    public static function generateSlug($title){
+        $slug = Str::slug($title, '-');
+        $count = 1;
+        while(Project::where('slug', $slug)->first()){
+            $slug = Str::slug($title . '-' . $count, '-');
+            $count++;
+        }
         return $slug;
     }
 
