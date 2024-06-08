a434dd594352a6c0af8bdf980d2f3d4ce9bb82f7
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-06-08 19:20:03 +0200

    update relation with table technologies

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 use App\Models\Type;
+use App\Models\Technology;
 
 class Project extends Model
 {
@@ -23,5 +24,8 @@ class Project extends Model
     }
     public function type(){
         return $this->belongsTo(Type::class);
+    }
+    public function technologies(){
+        return $this->belongsToMany(Technology::class);
     }
 }
